e0efbbc4501a9c43b709239df89552f9d5d6d605
Md Younus Ahamed <42135612+shuvo14051@users.noreply.github.com>
2023-04-07 03:04:15 -0400

    Download images from Google using Python

--- download_images.py
+++ download_images.py
@@ -1,3 +1,9 @@
+"""
+pip install simple_image_download==0.4
+to make this work must download this version
+latest version does not work
+"""
+
 from simple_image_download import simple_image_download as simp
 
 response = simp.simple_image_download
